a5f80adce50a73a14250b44d0b1e27af0faeb6b1
Ornerd <ukoorbytal@gmail.com>
2025-10-25 02:20:47 +0100

    correctly adding my poppins font

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,9 @@
 <template>
-  <footer class="bg-gray-800 text-white py-8 px-4">
-    <div class="max-w-6xl mx-auto text-center">
-      <p>&copy; 2025 Ortel. All rights reserved.</p>
-    </div>
-  </footer>
+  <footer className="bg-[#163052] text-white py-8 px-4 w-full">
+        <div className="max-w-6xl mx-auto text-center">
+          <p>&copy; 2025 Ortel. All rights reserved.</p>
+        </div>
+    </footer>
 </template>
 
 <script>
